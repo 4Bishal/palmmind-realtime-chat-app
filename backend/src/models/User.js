9f82a2e5860d4c8ab5a4429b87f6f9f76ffbb004
Bishal Bhandari <bhandarisuraksha617@gmail.com>
2026-05-18 18:00:51 +0545

    Add bcrypt password hashing and comparison method

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema(
     {
@@ -24,6 +25,23 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+
+// Method to compare password during login
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 const User = mongoose.model("User", userSchema);
 
